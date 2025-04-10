8e70ef4f9ce70c1105a43445afebdcb5eba9eb89
Ife <ifeanyialutu@outlook.com>
2025-04-10 15:12:14 -0400

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -1,11 +1,38 @@
+# https://github.com/Ife-A/lab10-IA-AU
+# Partner 1: Ifeanyichukwu Alutu
+# Partner 2: Ayan Uzzyman
+
 """
 calculator.py
 - Defines functions used to create a simple calculator
 
 One function per operation, in order.
 """
+import math
 # First example
 def add(a, b): 
-    pass
+    return a+b
+
+
+def subtract(a,b):
+    return a-b
+
+def multiply(a,b):
+    return a*b
+
+def divide(a,b):
+    if a == 0:
+        raise ZeroDivisionError
+
+    return b/a
+
+def logarithm(a,b):
+    if a < 1 or b < 1:
+        raise ValueError
+
+    return math.log(b,a)
+
+def exponent(a,b):
+    return a**b
 
 
